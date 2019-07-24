26b6840899e1b393d59ee966cc432a6328a3c4f7
Niku Hietanen <niku.hietanen@zeelandfamily.fi>
2019-07-24 12:45:13 +0300

    Add comments, wrap to a function, add support to legacy browsers

--- js/chart.js
+++ js/chart.js
@@ -1,22 +1,42 @@
-const avidlyFrappeCharts = document.querySelectorAll( '.avidly-frappe-chart' );
-for (let index = 0; index < avidlyFrappeCharts.length; index++) {
-	const element = avidlyFrappeCharts[index];
-	// Get the id
-	const id = element.dataset.frappeId;
-	// Get the localized data
-	const settings = window[ 'avidlyFrappeChart_' + id ];
-	let chart = new frappe.Chart( element, {
-		data: JSON.parse( settings.data ),
-		axisOptions: {
-			xIsSeries: true,
-			xAxisMode: 'tick',
-		},
-		lineOptions: {
-			hideDots: 1,
-		},
-		title: settings.title,
-		type: 'axis-mixed', // or 'bar', 'line', 'pie', 'percentage'
-		height: 300,
-		colors: ['#107737', '#F08627', '#4E96EF', '#874982', ],
-	});
-}
+/**
+ * Initialize all Frappe charts found in DOM
+ */
+(function() {
+
+	// Select all charts in a page
+	var avidlyFrappeCharts = document.querySelectorAll( '.avidly-frappe-chart' );
+
+	// Loop over charts
+	for (var index = 0; index < avidlyFrappeCharts.length; index++) {
+
+		var element = avidlyFrappeCharts[index];
+
+		// Get the id
+		var id = element.dataset.frappeId ? element.dataset.frappeId : 0;
+
+		// Get the localized data
+		var settings = window[ 'avidlyFrappeChart_' + id ] ? window[ 'avidlyFrappeChart_' + id ] : [];
+
+		// Jump out if settings is empty
+		if ( ! settings.data.length ) {
+			continue;
+		}
+
+		// Initialize the chart
+		var chart = new frappe.Chart( element, {
+			data: JSON.parse( settings.data ),
+			axisOptions: {
+				xIsSeries: true,
+				xAxisMode: 'tick',
+			},
+			lineOptions: {
+				hideDots: 1,
+			},
+			title: settings.title,
+			type: 'axis-mixed', // or 'bar', 'line', 'pie', 'percentage'
+			height: 300,
+			colors: ['#107737', '#F08627', '#4E96EF', '#874982', ],
+		});
+	}
+
+})(jQuery);
